4b028d404348743acf9d95562de9f9a57cf9b8ea
haeun <kodms08@naver.com>
2023-11-27 15:50:26 +0900

    feat: 엑세스토큰 로컬 스토리지 저장

--- assets/javascripts/authorize.js
+++ assets/javascripts/authorize.js
@@ -4,12 +4,10 @@ const GITHUB_CLIENT_SECRET = '45ef1ad0380204292293bacc888173c17d039ad5';
 
 // url 인가코드 파싱
 function parseAccessCode(url) {
-  console.log('인가코드 파싱하러 들어왔다?');
   if (url.match(/\?error=(.+)/)) {
     // TODO: 오류 알림을 띄우고 창을 닫아야함
     return null;
   } else {
-    console.log('인가코드가 있다?');
     // eslint-disable-next-line
     const accessCode = url.match(/\?code=([\w\/\-]+)/);
     if (accessCode) {
@@ -18,53 +16,42 @@ function parseAccessCode(url) {
   }
 }
 
-
 function getAccessToken(code) {
-  console.log('엑세스 코드 가지러 간다');
   fetch('https://github.com/login/oauth/access_token', {
     method: 'POST',
     headers: {
-      accept: 'application/json',
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
     },
-    body: {
+    body: JSON.stringify({
       client_id: CLIENT_ID,
       client_secret: GITHUB_CLIENT_SECRET,
-      code: code  
-    }
+      code: code,
+    }),
   })
     .then((response) => {
-      console.log(response);
+      response.json().then((data) => {
+        const accessToken = data.access_token;
+        chrome.storage.local.set({ cogit_token: accessToken });
+        // chrome.storage.local.get(['cogit_token'],(data) => {
+        //   console.log(data.cogit_token);  
+        // })
+      });
     })
-    // .then(response => response.json())
-    // .then(data => {
-      
-    //   const accessToken = data.access_token
-    //   console.log('Access Token:', accessToken);
-    // })
-    .catch(error => {
+    .catch((error) => {
       console.error('Error:', error);
-    })
-
+    });
 }
 /* Check for open pipe */
-if (
-  window.location.host === 'github.com' &&
-  window.location.href.includes('?code=')
-) {
-  console.log('깃허브고 코드가 있다?');
+if (window.location.host === 'github.com' && window.location.href.includes('?code=')) {
   const link = window.location.href;
   // pipe_cogit 정보가 있으면 (현재 로그인 중이면)
   chrome.storage.local.get('pipe_cogit', (data) => {
     if (data && data.pipe_cogit) {
-      console.log('pipe_cogit이 있다?');
       const code = parseAccessCode(link);
       if (code != null) {
-        console.log('코드가 널이 아니다?');
-        console.log(code);
-        //requestCogitLogin(code);
+        // 인가코드를 통해, git api 요청(엑세스토큰)
         getAccessToken(code);
-
-        //TODO: 인가코드를 통해, git api 요청(엑세스토큰)
       }
     }
   });
