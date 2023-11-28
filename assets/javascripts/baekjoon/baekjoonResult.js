cc1a252a9bb94055161217fe4d2aba916025e8fa
haeun <kodms08@naver.com>
2023-11-28 10:21:01 +0900

    refactor: 백준 코깃 비활성화

--- assets/javascripts/baekjoon/baekjoonResult.js
+++ assets/javascripts/baekjoon/baekjoonResult.js
@@ -1,102 +1,100 @@
 const PLATFORM_URL = 'https://www.acmicpc.net/problem/';
 
-async function checkActive() {
+function checkActive(callback) {
   //코깃 활성화 여부
-  chrome.storage.local.get('active').then(async (result) => {
-    if (result.active) {
-      console.log('???');
-      if (result.active == 'active') {
-        console.log('active');
-        return true;
-      } else {
-        return false;
-      }
+  chrome.storage.local.get('active', function (result) {
+    if (result.active && result.active == 'active') {
+      callback(true);
     } else {
-      return false;
+      callback(false);
     }
   });
 }
-let isActive = checkActive();
-console.log(isActive);
 
-if (isActive && localStorage.getItem('code')) {
-  const code = localStorage.getItem('code'); //제출한 코드 가지고 오기
-  localStorage.removeItem('code');
+function getResult(isActive) {
+  if (isActive && localStorage.getItem('code')) {
+    const code = localStorage.getItem('code'); //제출한 코드 가지고 오기
+    localStorage.removeItem('code');
 
-  let firstSolutionElement = document.querySelector('[id^="solution-"]');
-  let resultElement = firstSolutionElement.querySelector('.result');
-  let preContent = resultElement.querySelector('span').textContent;
+    let firstSolutionElement = document.querySelector('[id^="solution-"]');
+    let resultElement = firstSolutionElement.querySelector('.result');
+    let preContent = resultElement.querySelector('span').textContent;
 
-  if (preContent.includes('')) {
-    firstSolutionElement = document.querySelector('[id^="solution-"]');
-    resultElement = firstSolutionElement.querySelector('.result');
-    let loadingImg = document.createElement('img');
-    loadingImg.src = 'https://cogitusercode.s3.ap-northeast-2.amazonaws.com/assets/loading.gif';
-    loadingImg.style = 'width:20px';
-    resultElement.appendChild(loadingImg);
-
-    // 2초 간격으로 내용 확인
-    const intervalId = setInterval(function () {
+    if (preContent.includes('중')) {
       firstSolutionElement = document.querySelector('[id^="solution-"]');
       resultElement = firstSolutionElement.querySelector('.result');
-      let currentContent = resultElement.querySelector('span').textContent;
+      let loadingImg = document.createElement('img');
+      loadingImg.src = 'https://cogitusercode.s3.ap-northeast-2.amazonaws.com/assets/loading.gif';
+      loadingImg.style = 'width:20px';
+      resultElement.appendChild(loadingImg);
 
-      if (currentContent === preContent && !currentContent.includes('중')) {
-        //채점 완료
-        clearInterval(intervalId); // setInterval 중지
-        let codeLanguage = firstSolutionElement
-          .querySelector('td:nth-child(7)')
-          .querySelector('a')
-          .textContent.trim();
-        let codeFileExtension = baekjoonExtension[codeLanguage];
-        codeLanguage = baekjoonLanguages[codeLanguage];
-        let codeRunningTime = firstSolutionElement.querySelector('.time').textContent;
-        let algorithmQuestId = firstSolutionElement
-          .querySelector('td:nth-child(3)')
-          .querySelector('a').textContent;
-        // let algorithmName = firstSolutionElement
-        //   .querySelector('td:nth-child(3)')
-        //   .querySelector('a')
-        //   .querySelector('data-originar-title');
-        // console.log(algorithmName);
+      // 2초 간격으로 내용 확인
+      const intervalId = setInterval(function () {
+        firstSolutionElement = document.querySelector('[id^="solution-"]');
+        resultElement = firstSolutionElement.querySelector('.result');
+        let currentContent = resultElement.querySelector('span').textContent;
 
-        if (currentContent.includes('맞았습니다')) {
-          loadingImg.remove();
-          var cogitImg = document.createElement('img');
-          cogitImg.src = 'https://cogitusercode.s3.ap-northeast-2.amazonaws.com/assets/cogit.png';
-          cogitImg.style = 'width:20px; margin-left:5px';
+        if (currentContent === preContent && !currentContent.includes('중')) {
+          //채점 완료
+          clearInterval(intervalId); // setInterval 중지
 
-          resultElement.appendChild(cogitImg);
-          console.log(code);
-          console.log(codeLanguage);
-          console.log(codeRunningTime);
-          console.log(algorithmQuestId);
-          // console.log(algorithmQuestName);
-          console.log(codeFileExtension);
+          let codeLanguage = firstSolutionElement
+            .querySelector('td:nth-child(7)')
+            .querySelector('a')
+            .textContent.trim();
+          let codeFileExtension = baekjoonExtension[codeLanguage];
+          codeLanguage = baekjoonLanguages[codeLanguage];
+          let codeRunningTime = firstSolutionElement.querySelector('.time').textContent + 'ms';
+          let algorithmQuestId = firstSolutionElement
+            .querySelector('td:nth-child(3)')
+            .querySelector('a').textContent;
+          let algorithmName = firstSolutionElement
+            .querySelector('td:nth-child(3)')
+            .querySelector('a')
+            .getAttribute('data-original-title');
 
-          // sendCode(
-          //   code,
-          //   true,
-          //   'BAEKJOON',
-          //   codeLanguage,
-          //   codeRunningTime,
-          //   algorithmQuestId,
-          //   codeFileExtension,
-          //   `${PLATFORM_URL}${algorithmQuestId}`
-          // );
+          if (currentContent.includes('맞았습니다')) {
+            loadingImg.remove();
+            var cogitImg = document.createElement('img');
+            cogitImg.src = 'https://cogitusercode.s3.ap-northeast-2.amazonaws.com/assets/cogit.png';
+            cogitImg.style = 'width:20px; margin-left:5px';
+
+            resultElement.appendChild(cogitImg);
+            console.log(code);
+            console.log(codeLanguage);
+            console.log(codeRunningTime);
+            console.log(algorithmQuestId);
+            console.log(algorithmName);
+            console.log(codeFileExtension);
+
+            // sendCode(
+            //   code,
+            //   true,
+            //   'BAEKJOON',
+            //   codeLanguage,
+            //   codeRunningTime,
+            //   algorithmQuestId,
+            //   codeFileExtension,
+            //   `${PLATFORM_URL}${algorithmQuestId}`
+            // );
+          } else {
+            loadingImg.remove();
+            var cogitGreyImg = document.createElement('img');
+            cogitGreyImg.src =
+              'https://cogitusercode.s3.ap-northeast-2.amazonaws.com/assets/cogit_gray.png';
+            cogitGreyImg.style = 'width:20px; margin-left:5px';
+            resultElement.appendChild(cogitGreyImg);
+          }
         } else {
-          loadingImg.remove();
-          var cogitGreyImg = document.createElement('img');
-          cogitGreyImg.src =
-            'https://cogitusercode.s3.ap-northeast-2.amazonaws.com/assets/cogit_gray.png';
-          cogitGreyImg.style = 'width:20px; margin-left:5px';
-          resultElement.appendChild(cogitGreyImg);
+          preContent = currentContent; // 현재 내용을 저장
         }
-      } else {
-        preContent = currentContent; // 현재 내용을 저장
-      }
-    }, 2000);
+      }, 2000);
+    } else {
+      console.log('이미 전송한 코드입니다.');
+    }
   } else {
-    console.log('이미 전송한 코드입니다.');
+    localStorage.removeItem('code');
   }
 }
+
+checkActive(getResult);
